130c6a5ed3ab0745ffc3e6463f02f30a06f7151b
Abdullah khidr <Abdullahkhidrmain@gmail.com>
2024-03-30 17:59:13 +0200

    Responsive Size Board

--- app/src/main/java/x/bod/game/tootoo/MainActivity.kt
+++ app/src/main/java/x/bod/game/tootoo/MainActivity.kt
@@ -7,10 +7,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import x.bod.game.tootoo.components.GameController
 import x.bod.game.tootoo.components.GameStatusController
@@ -31,7 +35,7 @@ class MainActivity : ComponentActivity() {
 fun GameScreen() {
     TOOTOOTheme {
         Box(modifier = Modifier.background(PrimaryColor)) {
-            Column {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 Row(modifier = Modifier.padding(top = 15.dp, start = 15.dp)) {
                     GameStatusController()
                 }
@@ -39,6 +43,7 @@ fun GameScreen() {
                     activePoints = GameSettings.activePoints,
                     modifier = Modifier
                         .weight(1f)
+                        .aspectRatio(1f/2f)
                         .padding(vertical = 20.dp, horizontal = 20.dp)
                 )
                 GameController()
